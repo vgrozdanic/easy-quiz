44d05c1eba21881b4c40c8e1ff2fa40f00b664d0
Vjeran Grozdanic <vjeran.grozdanic@gmail.com>
2023-11-11 20:13:52 +0100

    play and create quiz working version
    Signed-off-by: Vjeran Grozdanic <vjeran.grozdanic@gmail.com>

--- script.js
+++ script.js
@@ -34,3 +34,91 @@ function addQuestion() {
 
   questionsContainer.appendChild(newQuestionDiv);
 }
+
+function submitForm() {
+  // Build JSON object from the form data
+  quizData = [];
+  for (let i = 1; i <= questionCounter; i++) {
+    const question = document.getElementById(`question${i}`).value;
+    const answer = document.getElementById(`answer${i}`).value;
+    quizData.push({ question, answer });
+  }
+
+  // Encode the JSON object in base64
+  const base64QuizData = btoa(JSON.stringify(quizData));
+
+  // Add the base64-encoded data as a URL parameter
+  window.location.href = `?data=${base64QuizData}`;
+
+  // Prevent the form from actually submitting
+  return false;
+}
+
+function renderPlayQuizQuestions(quizData) {
+  const quizQuestions = JSON.parse(atob(quizData));
+  console.log(quizQuestions);
+
+  const playQuizContainer = document.getElementById("playQuizContainer");
+
+  quizQuestions.forEach((quizQuestion, idx) => {
+    const newQuestionDiv = document.createElement("div");
+    newQuestionDiv.classList.add("playQuizQuestion");
+
+    const labelQuestion = document.createElement("playQuizLabel");
+    labelQuestion.htmlFor = `playQuizQuestion${idx}`;
+    labelQuestion.textContent = quizQuestion.question;
+    newQuestionDiv.appendChild(labelQuestion);
+
+    const inputAnswer = document.createElement("input");
+    inputAnswer.type = "text";
+    inputAnswer.id = `playQuizAnswer${idx}`;
+    inputAnswer.name = `playQuizAnswer${idx}`;
+    inputAnswer.required = true;
+    newQuestionDiv.appendChild(inputAnswer);
+
+    playQuizContainer.appendChild(newQuestionDiv);
+  });
+}
+
+function submitPlayQuiz() {
+  const urlParams = new URLSearchParams(window.location.search);
+  quizData = urlParams.get("data");
+  const quizQuestions = JSON.parse(atob(quizData));
+
+  let score = 0;
+  quizQuestions.forEach((quizQuestion, idx) => {
+    const userAnswer = document.getElementById(`playQuizAnswer${idx}`).value;
+    if (userAnswer === quizQuestion.answer) {
+      score++;
+    }
+  });
+
+  const playQuizResult = document.getElementById("playQuizResult");
+  playQuizResult.style.display = "block";
+
+  const playQuizResultText = document.getElementById("playQuizResultText");
+  playQuizResultText.textContent = `You scored ${score} out of ${quizQuestions.length}!`;
+
+  // prevent the form from actually submitting
+  return false;
+}
+
+window.onload = function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  quizData = urlParams.get("data");
+
+  if (quizData !== null) {
+    // if there is data in the URL, show the quiz, not create quiz form
+    const createQuizDiv = document.getElementById("createQuizDiv");
+    createQuizDiv.style.display = "none";
+    const createQuizH1 = document.getElementById("createQuizH1");
+    createQuizH1.style.display = "none";
+
+    const playQuizDiv = document.getElementById("playQuizDiv");
+    playQuizDiv.style.display = "block";
+    const playQuizH1 = document.getElementById("playQuizH1");
+    playQuizH1.style.display = "block";
+
+    renderPlayQuizQuestions(quizData);
+  }
+};
